2d08d5624c58fbd89b75b894aa146a1836698063
Clarastar <clarastar1788@gmail.com>
2016-10-03 20:22:37 +0800

    单词网站注册界面制作

--- header.php
+++ header.php
@@ -7,41 +7,37 @@ header('Content-Type: text/html;charset=UTF-8');
         <li><a href="#">我的收藏</a></li>
         <li><a href="#">单词游戏</a></li>
         <li><a href="#" id="btLogin">登陆</a>
+        </li>
+        <li><a href="regist.html" id="btReg">注册</a>
+
+        </li>
+    </ul>
+
             <div class="modal" id="modalLogin">
                 <div class="modal-dialog">
                     <div class='modal-content'>
-                        <h3>你好，欢迎登陆！</h3>
-                        <span>用户名：</span><input id="uname" placeholder="请输入用户名"><br>
-                        <span>密码：</span><input id="upwd" placeholder="请输入密码"><br>
-                        <button class='btnLog' id="btnLog">登陆</button>
-                    </div>
-                </div>
-            </div>
-        </li>
-        <li><a href="#" id="btReg">注册</a>
-            <div class="modal" id="modalRegister">
-                <div class="modal-dialog">
-                    <div class='modal-content'>
-                        <form action="#">
-                            <h3>你好，欢迎注册！</h3>
-                            <span>用户名：</span>
-                            <input id="uname" placeholder="请输入用户名"><br>
+                            <h4>你好，欢迎登陆！</h4>
+                        <form id="login-form" action="#">
+                            <div class="form-group">
+                                <label class="col-md-2 control-label" for="username">用户名:</label>
+                                <div class="col-md-5">
+                                <input id="username" name="uname" class="form-control" autofocus placeholder="请输入用户名">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-2 control-label" for="password">密码:</label>
+                                <div class="col-md-5">
+                                <input id="password" name="upwd" type="password" class="form-control" placeholder="请输入密码">
+                                </div>
+                            </div>
 
-                            <span>密码：</span>
-                            <input type="password"id="upwd" placeholder="请输入密码"><br>
-
-                            <span>密码：</span>
-                            <input type="password" autofocus id="upwd" placeholder="请再次输入密码"><br>
-
-                            <span>邮箱：</span>
-                            <input type="email"><br>
-
-                            <button type="submit" class='btnReg'id="btnReg">注册</button>
-
+                            <div class="form-group">
+                                <label class="col-md-2 control-label"></label>
+                                <div class="col-md-5">
+                                    <input id="btnLog" type="submit" value="提交" class="btn btn-success btn-block">
+                                </div>
+                            </div>
                         </form>
-
                     </div>
                 </div>
             </div>
-        </li>
-    </ul>
