8ac2b2a07025f227f3cfc7d67e6ab4e030127bb1
Clarastar <clarastar1788@gmail.com>
2016-10-18 21:40:06 +0800

    主页的“开始”按钮js更改

--- js/index.js
+++ js/index.js
@@ -1,10 +1,24 @@
 /***页面背景的动态****/
 window.onscroll=function(){
-    var y=-5*window.pageYOffset+"px";
+    var y=-4*window.pageYOffset+"px";
     $("#header").animate({top:y},1);
     $(".videoBg").animate({top:y},1);
 };
 
 $(function(){
     $('#nav-bar-box').load('header.php');
+});
+
+
+$(".com-my-start a").mouseenter(function(){
+    $(this).animate({
+        backgroundColor: "#ff9933",
+        color:"#fff;"
+    }, 500 );
+});
+$(".com-my-start a").mouseleave(function(){
+    $(this).animate({
+        backgroundColor: "#ffe",
+        color:"#ff9933;"
+    }, 500 );
 });
